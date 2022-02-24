8d9842657deb3d8293386e0001265031284430fd
Tgarrey37 <tpopp0303@gmail.com>
2022-02-23 18:59:23 -0700

    Working on styling for post components and structure with react

--- drip/src/pages/home/Home.jsx
+++ drip/src/pages/home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Home.css'
-import Header from '../../header/Header'
+import Header from '../../header/header'
 import Posts from '../../posts/Posts'
 import SideBar from '../../sidebar/SideBar'
 
